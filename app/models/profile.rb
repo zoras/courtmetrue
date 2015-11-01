8ee0fa97a2e69994046a898239387ea29b05e65c
Savan Raval <savan_raval@yahoo.com>
2015-11-01 08:42:35 +0530

    check for profile completed or not

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,4 +8,31 @@ class Profile < ActiveRecord::Base
   def female?
     self.gender == "Female"
   end
+
+  def finish_basic_info?
+	  if 	self.name.blank? ||
+				self.age.blank? ||
+		  	self.dob.blank? ||
+		  	self.country.blank? ||
+		  	self.state.blank? ||
+		  	self.city.blank? ||
+		  	self.gender.blank? ||
+		  	self.status.blank?
+  		false
+  	else
+  		true
+  	end
+  end
+
+  def finish_personal_info?
+  	
+  end
+
+  def finish_additional_info?
+  	
+  end
+
+  def finished_profile?
+  	return finish_basic_info? && finish_personal_info? && finish_additional_info? ? true : false
+  end
 end
