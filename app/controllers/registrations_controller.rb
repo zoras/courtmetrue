add1171171929429801600c9d2161cbbd57dbb5b
Daniel Kehoe <daniel@danielkehoe.com>
2015-11-19 10:33:59 +0700

    fix after_sign_in_path so users see profile form after joining site

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    profile_path(:start_profile)
+    profile_path(:basic_info)
   end
 
   def after_sign_in_path_for(resource)
